1338e909a93f62f4d5d274385f8e99c609b645db
luisCavalcantiDev <luis.cavalcanti583@gmail.com>
2021-09-11 17:24:47 -0300

    Upload projeto corrigido

---
+++ backend/src/main/java/br/com/luisdev/sds/api/SdsapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisdev.sds.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/luisdev/sds/api/SdsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisdev.sds.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
